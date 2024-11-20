7d4c3d4b5a3edb7504d66300203841857be417ad
zbilamouette <84340541+zbilamouette@users.noreply.github.com>
2024-11-20 13:28:56 +0000

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,11 @@
+public class Q6 {
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+    
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) System.out.print("x");
+            System.out.println();
+        }
+    }
+}
