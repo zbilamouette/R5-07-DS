6eda1724984e724826026aa864ef8eb56a4c92d4
zbilamouette <84340541+zbilamouette@users.noreply.github.com>
2024-11-20 13:24:28 +0000

    Change triangle orientation

--- Q5.java
+++ Q5.java
@@ -4,7 +4,7 @@ public class Q5 {
         int n = Integer.parseInt(args[0]);
     
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) System.out.print("*");
+            for (int j = i; j <= n; j++) System.out.print("*");
             System.out.println();
         }
     }
