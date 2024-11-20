cc40f32ee67ad2020c6aa6b332b44528a5553345
zbilamouette <84340541+zbilamouette@users.noreply.github.com>
2024-11-20 13:05:31 +0000

    create Q3.java

---
+++ Q3.java
@@ -0,0 +1,9 @@
+public class Q3 {
+    public static void main(String[] args) {
+        int nb = Integer.parseInt(args[0]);
+        
+        for(int i=0;i<nb;i++) System.out.print("*");
+        
+        System.out.println();
+    }
+}
